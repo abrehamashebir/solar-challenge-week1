3e5ea78231894344cf7cc6b96404714784135b12
abrehamashebir <abrisha96@gmail.com>
2025-05-20 14:27:50 +0300

    fix: file path error in streamlit

--- app/utils.py
+++ app/utils.py
@@ -2,18 +2,11 @@ import pandas as pd
 import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-DATA_DIR = os.path.join(BASE_DIR, 'data')
+DATA_DIR = os.path.join(BASE_DIR, 'dataset')
 
 def load_data():
-    benin = pd.read_csv(os.path.join(DATA_DIR,'benin_clean.csv'))
-    sierra_leone = pd.read_csv(os.path.join(DATA_DIR,'sierraleone_clean.csv'))
-    togo = pd.read_csv(os.path.join(DATA_DIR,'togo_clean.csv'))
-
-    benin['Country'] = 'Benin'
-    sierra_leone['Country'] = 'Sierra Leone'
-    togo['Country'] = 'Togo'
-
-    df = pd.concat([benin, sierra_leone, togo], ignore_index=True)
+    df = pd.read_csv(os.path.join(DATA_DIR,'combined.csv'))
+    
     return df
 
 def get_summary_table(df):
