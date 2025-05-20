cf93b8730b2d86680e1ef3f72733d5828a05dfbd
abrehamashebir <abrisha96@gmail.com>
2025-05-20 13:10:42 +0300

    fix: file path error in streamlit

--- app/main.py
+++ app/main.py
@@ -3,9 +3,10 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 import os, sys
-current_path = os.getcwd()
-parent_dir = os.path.dirname(current_path)
-sys.path.insert(0, parent_dir)
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DATA_DIR = os.path.join(BASE_DIR, 'data')
 # Set the page configuration
 from utils import load_data, get_summary_table
 st.set_page_config(page_title="Solar Potential Dashboard", page_icon=":guardsman:", layout="wide")
